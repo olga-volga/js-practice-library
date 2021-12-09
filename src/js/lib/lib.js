e96b220bcf3e34a853be90d97102b55529830e60
olga <yaolmi@yandex.ru>
2021-12-09 16:22:09 +0700

    edit module name

--- src/js/lib/lib.js
+++ src/js/lib/lib.js
@@ -1,7 +1,7 @@
 import $ from './core';
 import './modules/display';
 import './modules/classes';
-import './modules/actions';
+import './modules/eventActions';
 import './modules/attributes';
 
 export default $;
